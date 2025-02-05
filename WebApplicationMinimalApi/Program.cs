0e9d69a3b817dcbc163ae23dbd0a9efb40d32f9e
Thomas Bakken Moe <thom@computas.com>
2025-02-05 14:36:33 +0100

    Adds Swagger header info

--- WebApplicationMinimalApi/Program.cs
+++ WebApplicationMinimalApi/Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
-
-
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Fruit API",
+        Description = "API for managing a list of fruit and their stock status.",
+        TermsOfService = new Uri("http://example.com/terms")
+    });
+});
+
 
 var app = builder.Build();
 
