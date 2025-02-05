1eda0c324350cea3c04a5987da772a4fdeec2f6b
Thomas Bakken Moe <thom@computas.com>
2025-02-05 14:28:51 +0100

    Adds Swagger

--- WebApplicationMinimalApi/Program.cs
+++ WebApplicationMinimalApi/Program.cs
@@ -1,9 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/users/{userID}/books/{bookId}",
     (int userId, int bookId) => $"The user id is {userId} and book id is {bookId}");
+
+app.UseSwagger();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwaggerUI();
+}
 
 app.Run();
